701df34ff9cc20f2a50faced693c5b3967f19326
TaslimOwolarafe <owolarafetaslim@gmail.com>
2022-07-11 10:30:51 +0100

    after mobile- first try

---
+++ app.js
@@ -0,0 +1,23 @@
+const companyDropdown = document.querySelector(".company-menu-wrap");
+const company = document.querySelector(".company");
+const features = document.querySelector(".features");
+const featuresDropdown = document.querySelector(".features-menu-wrap");
+const featuresArrow = document.querySelector(".features span img");
+const companyArrow = document.querySelector(".company span img");
+const menu = document.querySelector('#mobile-menu');
+const sideNav = document.querySelector(".side-nav");
+
+company.addEventListener("click", () => {
+    companyDropdown.classList.toggle("active");
+    companyArrow.classList.toggle("is-active")
+});
+
+features.addEventListener("click", () => {
+    featuresDropdown.classList.toggle("active");
+    featuresArrow.classList.toggle("is-active")
+});
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    sideNav.classList.toggle("pop-up")
+});
