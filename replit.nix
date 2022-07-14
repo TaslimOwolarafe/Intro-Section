c6226c07ebc1bafd213d9cbfc44090e75ba52de9
owolarafetaslim <>
2022-07-14 20:37:52 +0000

    modified styles

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
   deps = [
-    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.clear
+pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
 }
